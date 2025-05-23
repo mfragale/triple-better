1007d830604b30e480a3388ecdf25d2764f5058e
Mauricio Fragale <mfragale@gmail.com>
2025-05-23 01:19:10 -0300

    Implement inline editing for todos in Checklist component, allowing users to update todo text directly. Refactor layout to improve usability and maintainability.

--- src/components/checklist.tsx
+++ src/components/checklist.tsx
@@ -4,16 +4,13 @@ import { triplitClient } from "@/lib/triplit";
 import { cn } from "@/lib/utils";
 import { useQuery } from "@triplit/react";
 import { Check, Square } from "lucide-react";
+import { useState } from "react";
 import { AddTodoForm } from "./add-todo-form";
 import { SortableItem, SortableList } from "./sortable-list";
 import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 
 export default function Checklist() {
-  // const { results: todos } = useQuery(
-  //   triplitClient,
-  //   triplitClient.query("todos").Order("created_at", "ASC")
-  // );
-
   const { results: todos } = useQuery(
     triplitClient,
     triplitClient.query("todos").Order("order", "ASC")
@@ -30,48 +27,34 @@ export default function Checklist() {
 
     return { error: false, message: "Order updated" };
   };
-  return (
-    <>
-      <p>Next item order: {todos?.length}</p>
-      <SortableList items={todos ?? []} onOrderChange={updateOrder}>
-        {(todos) => (
-          <div className="flex flex-col gap-2 mx-auto px-4 py-12 max-w-xl">
-            {todos.map((todo) => (
-              <SortableItem key={todo.id} id={todo.id}>
-                <div className="flex items-center gap-2">
-                  <Button
-                    variant="ghost"
-                    size="icon"
-                    onClick={() => {
-                      triplitClient.update("todos", todo.id, {
-                        completed: !todo.completed,
-                      });
-                    }}>
-                    {todo.completed ? (
-                      <Check className="text-green-500" />
-                    ) : (
-                      <Square />
-                    )}
-                  </Button>
-                  <span
-                    className={cn(
-                      todo.completed && "line-through text-muted-foreground/30"
-                    )}>
-                    {todo.text}
-                  </span>
-                </div>
-              </SortableItem>
-            ))}
-            <AddTodoForm nextItemIndex={todos?.length} />
-          </div>
-        )}
-      </SortableList>
 
-      {/* <div className="flex flex-col gap-2 mx-auto px-4 py-12 max-w-xl">
-        {todos?.map((todo) => (
-          <Card key={todo.id} className="group py-2">
-            <CardHeader className="flex justify-between items-center px-2">
-              <div className="flex items-center gap-2">
+  const [isEditing, setIsEditing] = useState<string | null>(null);
+
+  const handleTodoUpdate = async (formData: FormData) => {
+    console.log(formData);
+
+    const text = formData.get("text");
+    const id = formData.get("id");
+
+    if (!text) return;
+
+    triplitClient
+      .update("todos", id as string, {
+        text: text as string,
+      })
+      .then(async () => {
+        await new Promise((resolve) => setTimeout(resolve, 100));
+        setIsEditing(null);
+      });
+  };
+
+  return (
+    <SortableList items={todos ?? []} onOrderChange={updateOrder}>
+      {(todos) => (
+        <div className="flex flex-col gap-2 mx-auto px-4 py-12 max-w-xl">
+          {todos.map((todo) => (
+            <SortableItem key={todo.id} id={todo.id}>
+              <div className="flex items-center gap-2 grow">
                 <Button
                   variant="ghost"
                   size="icon"
@@ -86,48 +69,39 @@ export default function Checklist() {
                     <Square />
                   )}
                 </Button>
-                <span
-                  className={cn(
-                    todo.completed && "line-through text-muted-foreground/30"
-                  )}>
-                  {todo.text}
-                </span>
-              </div>
-
-              <Popover>
-                <PopoverTrigger asChild>
-                  <Button
-                    variant="ghost"
-                    size="icon"
-                    className="opacity-0 group-hover:opacity-100 text-muted-foreground hover:text-destructive transition-opacity">
-                    <Trash2 />
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-80">
-                  <div className="gap-4 grid">
-                    <div className="space-y-2">
-                      <h4 className="font-medium leading-none">
-                        Are you sure?
-                      </h4>
-                      <p className="text-muted-foreground text-sm">
-                        This action cannot be undone.
-                      </p>
-                    </div>
-                    <Button
-                      variant="destructive"
-                      onClick={() => {
-                        triplitClient.delete("todos", todo.id);
-                      }}>
-                      Delete
-                    </Button>
+                {isEditing === todo.id ? (
+                  <form action={handleTodoUpdate} className="w-full">
+                    <input type="hidden" name="id" value={todo.id} />
+                    <Input
+                      autoFocus
+                      type="text"
+                      name="text"
+                      onBlur={(e) => {
+                        const form = e.target.form;
+                        if (form) form.requestSubmit();
+                      }}
+                      defaultValue={todo.text}
+                      className="border-none focus-visible:ring-0 w-full md:text-base"
+                    />
+                  </form>
+                ) : (
+                  <div
+                    onClick={() => {
+                      setIsEditing(todo.id);
+                    }}
+                    className={cn(
+                      "w-full cursor-text pl-3",
+                      todo.completed && "line-through text-muted-foreground/30"
+                    )}>
+                    {todo.text}
                   </div>
-                </PopoverContent>
-              </Popover>
-            </CardHeader>
-          </Card>
-        ))}
-        <AddTodoForm />
-      </div> */}
-    </>
+                )}
+              </div>
+            </SortableItem>
+          ))}
+          <AddTodoForm nextItemIndex={todos?.length} />
+        </div>
+      )}
+    </SortableList>
   );
 }
