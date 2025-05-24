0491c3df4b9cb50b25f4f617e4b8e93296fa860a
Mauricio Fragale <mfragale@gmail.com>
2025-05-24 00:34:33 -0300

    Refactor AddTodoForm to use React Hook Form and Zod for form validation
    - Implemented form handling with `react-hook-form` and validation using `zod`.
    - Updated the form structure to utilize new UI components for better user experience.
    - Removed the previous form submission logic and replaced it with a more robust solution.

--- src/components/add-todo-form.tsx
+++ src/components/add-todo-form.tsx
@@ -1,49 +1,82 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { ArrowRight, Plus } from "lucide-react";
 import { useTranslations } from "next-intl";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { triplitClient } from "../../triplit/client";
 import { Card, CardHeader } from "./ui/card";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
 
 export function AddTodoForm({ nextItemIndex }: { nextItemIndex: number }) {
-  const handleSubmit = async (formData: FormData) => {
-    const addItem = formData.get("addItem");
-    if (!addItem) return;
-    // console.log(addItem);
+  const newTodoFormSchema = z.object({
+    newTodoItem: z.string().min(2).max(50),
+  });
+
+  const form = useForm<z.infer<typeof newTodoFormSchema>>({
+    resolver: zodResolver(newTodoFormSchema),
+    defaultValues: {
+      newTodoItem: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof newTodoFormSchema>) {
+    const newTodoItem = values.newTodoItem;
+
+    if (!newTodoItem) return;
+
     try {
       const insertedTodo = await triplitClient.insert("todos", {
-        text: addItem as string,
+        text: newTodoItem,
         order: nextItemIndex,
       });
+      await new Promise((resolve) => setTimeout(resolve, 200));
+      form.reset();
     } catch (error) {
       console.error(error);
     }
-  };
+  }
 
   const t = useTranslations("AddTodoForm");
 
   return (
-    <form action={handleSubmit}>
-      <Card className="bg-muted dark:bg-zinc-950 py-2 has-[input:focus]:ring has-[input:focus]:ring-indigo-500">
-        <CardHeader className="flex justify-between items-center px-2">
-          <div className="flex items-center gap-2 w-full">
-            <Button variant="ghost" size="icon" type="button">
-              <ArrowRight />
-            </Button>
-            <input
-              placeholder={t("addItemPlaceholder")}
-              className="bg-transparent -my-2 pl-3 focus:outline-none w-full h-12"
-              type="text"
-              name="addItem"
-            />
-          </div>
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <Card className="bg-muted dark:bg-zinc-950 py-2 has-[input:focus]:ring has-[input:focus]:ring-indigo-500">
+            <CardHeader className="flex justify-between items-start px-2">
+              <div className="flex items-start gap-2 w-full">
+                <Button variant="ghost" size="icon" type="button">
+                  <ArrowRight />
+                </Button>
+                <FormField
+                  control={form.control}
+                  name="newTodoItem"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormControl>
+                        <Input
+                          placeholder={t("addItemPlaceholder")}
+                          className="dark:bg-transparent -my-2 pt-2 pl-3 border-0 focus-visible:ring-0 focus:ring-0 dark:focus:ring-0 w-full h-12 md:text-base"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage className="pl-3" />
+                    </FormItem>
+                  )}
+                />
+              </div>
 
-          <Button size="icon" type="submit" variant="blue">
-            <Plus />
-          </Button>
-        </CardHeader>
-      </Card>
-    </form>
+              <Button size="icon" type="submit" variant="blue">
+                <Plus />
+              </Button>
+            </CardHeader>
+          </Card>
+        </form>
+      </Form>
+    </>
   );
 }
