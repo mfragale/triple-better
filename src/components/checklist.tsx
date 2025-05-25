f80c61ea55287ac0b88be0be515bc96eff0e2605
Mauricio Fragale <mfragale@gmail.com>
2025-05-24 23:43:42 -0300

    Enhance Checklist component with loading state and improved UI
    - Added loading state handling in the Checklist component using a Skeleton loader while fetching todos.
    - Refactored the rendering logic to maintain a clean structure and improve user experience during data fetching.
    - Ensured that the editing functionality remains intact while enhancing the overall layout and responsiveness.

--- src/components/checklist.tsx
+++ src/components/checklist.tsx
@@ -17,9 +17,10 @@ import { SortableItem, SortableList } from "./sortable-list";
 import { Button } from "./ui/button";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "./ui/form";
 import { Input } from "./ui/input";
+import { Skeleton } from "./ui/skeleton";
 
 export default function Checklist() {
-  const { results: todos } = useQuery(
+  const { results: todos, fetching } = useQuery(
     triplitClient,
     triplitClient.query("todos").Order("order", "ASC")
   );
@@ -64,81 +65,96 @@ export default function Checklist() {
     }
   }
 
+  if (fetching) {
+    return (
+      <div className="flex flex-col gap-2 mx-auto px-4 py-12 max-w-xl">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <div key={index} className="flex items-center gap-2 grow">
+            <Skeleton className="rounded-xl w-full h-[54px]" />
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   return (
-    <SortableList items={todos ?? []} onOrderChange={updateOrder}>
-      {(todos) => (
-        <div className="flex flex-col gap-2 mx-auto px-4 py-12 max-w-xl">
-          {todos.map((todo) => (
-            <SortableItem key={todo.id} id={todo.id}>
-              <div className="flex items-center gap-2 grow">
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  onClick={() => {
-                    triplitClient.update("todos", todo.id, {
-                      completed: !todo.completed,
-                    });
-                  }}>
-                  {todo.completed ? (
-                    <Check className="text-green-500" />
-                  ) : (
-                    <Square />
-                  )}
-                </Button>
-                {isEditing === todo.id ? (
-                  <Form {...form}>
-                    <form
-                      onSubmit={form.handleSubmit(onSubmit)}
-                      className="flex items-center w-full">
-                      <FormField
-                        control={form.control}
-                        name="editedTodoItemId"
-                        defaultValue={todo.id}
-                        render={({ field }) => (
-                          <Input type="hidden" {...field} />
-                        )}
-                      />
-                      <FormField
-                        control={form.control}
-                        name="editedTodoItem"
-                        defaultValue={todo.text}
-                        render={({ field }) => (
-                          <FormItem className="w-full">
-                            <FormControl>
-                              <Input
-                                autoFocus
-                                className="dark:bg-transparent -my-2 pt-1 pl-3 border-0 focus-visible:ring-0 focus:ring-0 dark:focus:ring-0 w-full h-12 md:text-base"
-                                {...field}
-                                onBlur={form.handleSubmit(onSubmit)}
-                              />
-                            </FormControl>
-                            <FormMessage className="pl-3" />
-                          </FormItem>
-                        )}
-                      />
-                      <Button type="submit" size="icon">
-                        <CornerDownLeft />
-                      </Button>
-                    </form>
-                  </Form>
-                ) : (
-                  <div
+    <>
+      <SortableList items={todos ?? []} onOrderChange={updateOrder}>
+        {(todos) => (
+          <div className="flex flex-col gap-2 mx-auto px-4 py-12 max-w-xl">
+            {todos.map((todo) => (
+              <SortableItem key={todo.id} id={todo.id}>
+                <div className="flex items-center gap-2 grow">
+                  <Button
+                    variant="ghost"
+                    size="icon"
                     onClick={() => {
-                      setIsEditing(todo.id);
-                    }}
-                    className={cn(
-                      "w-full cursor-text pl-3 ",
-                      todo.completed && "line-through text-muted-foreground/30"
-                    )}>
-                    {todo.text}
-                  </div>
-                )}
-              </div>
-            </SortableItem>
-          ))}
-          <AddTodoForm nextItemIndex={todos?.length} />
-        </div>
-      )}
-    </SortableList>
+                      triplitClient.update("todos", todo.id, {
+                        completed: !todo.completed,
+                      });
+                    }}>
+                    {todo.completed ? (
+                      <Check className="text-green-500" />
+                    ) : (
+                      <Square />
+                    )}
+                  </Button>
+                  {isEditing === todo.id ? (
+                    <Form {...form}>
+                      <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="flex items-center w-full">
+                        <FormField
+                          control={form.control}
+                          name="editedTodoItemId"
+                          defaultValue={todo.id}
+                          render={({ field }) => (
+                            <Input type="hidden" {...field} />
+                          )}
+                        />
+                        <FormField
+                          control={form.control}
+                          name="editedTodoItem"
+                          defaultValue={todo.text}
+                          render={({ field }) => (
+                            <FormItem className="w-full">
+                              <FormControl>
+                                <Input
+                                  autoFocus
+                                  className="dark:bg-transparent -my-2 pt-1 pl-3 border-0 focus-visible:ring-0 focus:ring-0 dark:focus:ring-0 w-full h-12 md:text-base"
+                                  {...field}
+                                  onBlur={form.handleSubmit(onSubmit)}
+                                />
+                              </FormControl>
+                              <FormMessage className="pl-3" />
+                            </FormItem>
+                          )}
+                        />
+                        <Button type="submit" size="icon">
+                          <CornerDownLeft />
+                        </Button>
+                      </form>
+                    </Form>
+                  ) : (
+                    <div
+                      onClick={() => {
+                        setIsEditing(todo.id);
+                      }}
+                      className={cn(
+                        "w-full cursor-text pl-3 ",
+                        todo.completed &&
+                          "line-through text-muted-foreground/30"
+                      )}>
+                      {todo.text}
+                    </div>
+                  )}
+                </div>
+              </SortableItem>
+            ))}
+            <AddTodoForm nextItemIndex={todos?.length} />
+          </div>
+        )}
+      </SortableList>
+    </>
   );
 }
